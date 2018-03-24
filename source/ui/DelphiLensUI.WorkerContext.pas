5bbd56b1e24458ebc650e19f3ff6e7be8079bc7c
Primoz Gabrijelcic <primoz@gabrijelcic.org>
2018-03-24 10:40:47 +0100

    context contains TSyntaxNode for current location

--- source/ui/DelphiLensUI.WorkerContext.pas
+++ source/ui/DelphiLensUI.WorkerContext.pas
@@ -4,15 +4,18 @@ interface
 
 uses
   Spring,
+  DelphiAST.Classes,
   DelphiLens.Intf,
   DelphilensUI.UIXStorage,
   DelphiLensUI.UIXEngine.Intf;
 
 type
   IDLUIWorkerContext = interface ['{D27B5BBF-B172-49ED-86BE-FCBC2CB80AFA}']
+    function  GetNamedSyntaxNode: TSyntaxNode;
     function  GetProject: IDLScanResult;
     function  GetSource: TDLUIXLocation;
     function  GetStorage: IDLUIXStorage;
+    function  GetSyntaxNode: TSyntaxNode;
     function  GetTarget: Nullable<TDLUIXLocation>;
     procedure SetTarget(const value: Nullable<TDLUIXLocation>);
   //
@@ -20,6 +23,8 @@ type
     property Project: IDLScanResult read GetProject;
     property Source: TDLUIXLocation read GetSource;
     property Target: Nullable<TDLUIXLocation> read GetTarget write SetTarget;
+    property SyntaxNode: TSyntaxNode read GetSyntaxNode;
+    property NamedSyntaxNode: TSyntaxNode read GetNamedSyntaxNode;
   end; { IDLUIWorkerContext }
 
 function CreateWorkerContext(const AStorage: IDLUIXStorage;
@@ -27,15 +32,23 @@ function CreateWorkerContext(const AStorage: IDLUIXStorage;
 
 implementation
 
+uses
+  DelphiAST.ProjectIndexer,
+  DelphiLens.DelphiASTHelpers;
+
 type
   TDLUIWorkerContext = class(TInterfacedObject, IDLUIWorkerContext)
   strict private
-    FStorage: IDLUIXStorage;
-    FProject: IDLScanResult;
-    FSource : TDLUIXLocation;
-    FTarget : Nullable<TDLUIXLocation>;
+    FNamedSyntaxNode: TSyntaxNode;
+    FProject        : IDLScanResult;
+    FSource         : TDLUIXLocation;
+    FStorage        : IDLUIXStorage;
+    FSyntaxNode     : TSyntaxNode;
+    FTarget         : Nullable<TDLUIXLocation>;
   strict protected
+    function  GetNamedSyntaxNode: TSyntaxNode;
     function  GetProject: IDLScanResult;
+    function  GetSyntaxNode: TSyntaxNode;
     function  GetSource: TDLUIXLocation;
     function  GetStorage: IDLUIXStorage;
     function  GetTarget: Nullable<TDLUIXLocation>;
@@ -47,6 +60,8 @@ type
     property Project: IDLScanResult read GetProject;
     property Source: TDLUIXLocation read GetSource;
     property Target: Nullable<TDLUIXLocation> read GetTarget write SetTarget;
+    property SyntaxNode: TSyntaxNode read GetSyntaxNode;
+    property NamedSyntaxNode: TSyntaxNode read GetNamedSyntaxNode;
   end; { TDLUIWorkerContext }
 
 { exports }
@@ -61,31 +76,49 @@ end; { CreateWorkerContext }
 
 constructor TDLUIWorkerContext.Create(const AStorage: IDLUIXStorage; const AProject:
   IDLScanResult; const ASource: TDLUIXLocation);
+var
+  unitInfo: TProjectIndexer.TUnitInfo;
 begin
   FStorage := AStorage;
   FProject := AProject;
   FSource := ASource;
+  if AProject.ParsedUnits.Find(ASource.UnitName, unitInfo)
+     and assigned(unitInfo.SyntaxTree)
+  then
+    FSyntaxNode := unitInfo.SyntaxTree.FindLocation(ASource.Line, ASource.Column);
+  if assigned(FSyntaxNode) then
+    FNamedSyntaxNode := FSyntaxNode.FindParentWithName;
 end; { TDLUIWorkerContext.Create }
+
+function TDLUIWorkerContext.GetNamedSyntaxNode: TSyntaxNode;
+begin
+  Result := FNamedSyntaxNode;
+end; { TDLUIWorkerContext.GetNamedSyntaxNode }
 
 function TDLUIWorkerContext.GetProject: IDLScanResult;
 begin
   Result := FProject;
-end;
+end; { TDLUIWorkerContext.GetProject }
 
 function TDLUIWorkerContext.GetSource: TDLUIXLocation;
 begin
   Result := FSource;
-end;
+end; { TDLUIWorkerContext.GetSource }
 
 function TDLUIWorkerContext.GetStorage: IDLUIXStorage;
 begin
   Result := FStorage;
-end;
+end; { TDLUIWorkerContext.GetStorage }
+
+function TDLUIWorkerContext.GetSyntaxNode: TSyntaxNode;
+begin
+  Result := FSyntaxNode;
+end; { TDLUIWorkerContext.GetSyntaxNode }
 
 function TDLUIWorkerContext.GetTarget: Nullable<TDLUIXLocation>;
 begin
   Result := FTarget;
-end;
+end; { TDLUIWorkerContext.GetTarget }
 
 procedure TDLUIWorkerContext.SetTarget(const value: Nullable<TDLUIXLocation>);
 begin

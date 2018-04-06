1140cfd9272f97a206e7883da66b2dd8f9332524
Primož Gabrijelčič <primoz@gabrijelcic.org>
2018-04-06 15:54:28 +0200

    null implementation

--- source/base/DelphiLens.Analyzers.Find.pas
+++ source/base/DelphiLens.Analyzers.Find.pas
@@ -9,11 +9,37 @@ function CreateDLFindAnalyzer(const scanResult: IDLScanResult): IDLFindAnalyzer;
 
 implementation
 
+uses
+  Spring.Collections,
+  DelphiLens.UnitInfo;
+
+type
+  TDLFindAnalyzer = class(TInterfacedObject, IDLFindAnalyzer)
+  strict private
+    FScanResult: IDLScanResult;
+  public
+    constructor Create(const scanResult: IDLScanResult);
+    function All(const ident: string): ICollection<TDLUnitCoordinates>;
+  end; { TDLUnitAnalyzer }
+
 { exports }
 
 function CreateDLFindAnalyzer(const scanResult: IDLScanResult): IDLFindAnalyzer;
 begin
-  Result := nil;
+  Result := TDLFindAnalyzer.Create(scanResult);
 end; { CreateDLFindAnalyzer }
+
+{ TDLFindAnalyzer }
+
+function TDLFindAnalyzer.All(const ident: string): ICollection<TDLUnitCoordinates>;
+begin
+  Result := TCollections.CreateList<TDLUnitCoordinates>;
+end; { TDLFindAnalyzer.All }
+
+constructor TDLFindAnalyzer.Create(const scanResult: IDLScanResult);
+begin
+  inherited Create;
+  FScanResult := scanResult;
+end; { TDLFindAnalyzer.Create }
 
 end.
